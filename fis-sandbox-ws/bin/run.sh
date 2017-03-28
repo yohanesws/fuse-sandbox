965b61e82c111fda3fe60ee6eed2b3f5cd59d08f
Yohanes Widi Sono <yohanesws@Yohaness-MacBook-Pro.local>
2017-03-28 10:56:25 +0800

    custom docker images

---
+++ fis-sandbox-ws/bin/run.sh
@@ -0,0 +1,1 @@
+java -XX:+UseParallelGC -XX:MinHeapFreeRatio=20 -XX:MaxHeapFreeRatio=40 -XX:GCTimeRatio=4 -XX:AdaptiveSizePolicyWeight=90 -XX:MaxMetaspaceSize=100m -cp . -jar ${JAVA_APP_DIR}/${JAVA_APP_NAME}
